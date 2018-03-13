cfa810c2232bd1b4149c947a10e8c6c797ad8f3d
vizhivsb <31505964+vizhivsb@users.noreply.github.com>
2018-03-13 10:41:50 +0000

    Create strnum.c

---
+++ strnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int n;
+scanf("%s",&n);
+if((n>=47)&&(n<=58))
+{
+printf("the string is numeric");
+	
+}
+else
+{
+printf("the string is not numeric");
+}
+return 0;
+}
